9cc246f86644e4e99341208017b4a80487253a65
Ahmed Ramzy <46076630+AhmedRamzyy@users.noreply.github.com>
2019-05-05 14:05:29 +0200

    smooth scroll added

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    'use strict';
-    //deal with tabs
-    $('.tab-switch li').click(function () {
-        $(this).addclass('selected').siblings().removeclass('selected');
-    })
-})
